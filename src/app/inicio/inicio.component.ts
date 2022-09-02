8521cd42c5881af4a6235002a375df28272a413b
jimalaros <jimalaros25@gmail.com>
2022-09-02 09:39:01 -0500

    first commit

--- src/app/inicio/inicio.component.ts
+++ src/app/inicio/inicio.component.ts
@@ -1,9 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'inicio',
-  templateUrl: './inicio.component.html',
-  styleUrls: ['./inicio.component.css']
+  selector: "inicio-component",
+  templateUrl: "./inicio.component.html",
+  styleUrls: ["./inicio.component.css"]
 })
 export class InicioComponent implements OnInit {
 
