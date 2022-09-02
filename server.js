6583babdae5df3fee7a81298038b8026a8ff1063
jimalaros <jimalaros25@gmail.com>
2022-09-02 07:54:13 -0500

    first commit

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ app.use(express.static(__dirname + '/dist/venta-autos'));
 
 // Send all requests to index.html
 app.get('/*', function(req, res) {
-  res.sendFile(path.join(__dirname + '/dist/VENTAAUTOS'));
+  res.sendFile(path.join(__dirname + '/dist/VENTAAUTOS/venta-autos'));
 });
 
 // default Heroku port
